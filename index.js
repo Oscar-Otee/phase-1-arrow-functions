60ca24ccfa1e98dc1feca6c2f33664fba288e512
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-09 11:44:26 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,14 @@
+// A function expression that returns the result of 2000 divide by 100.
+const divide = function () {
+    return 2000 / 100;
+}
+
+// An arrow function that returns the square of a number.
+const square = x => x **2;
+// An arrow function that adds two numbers and return the result.
+
+
+const add = (a, b) => a + b;
+
+
+console.log(add(23, 45));
